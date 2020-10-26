1758136eae361716b9703794b08321f386230761
QuXing9 <40267809+QuXing9@users.noreply.github.com>
2020-10-26 11:41:10 +0800

    update

---
+++ javaweb-02-servelt/servlet-01/src/main/java/com/xing/servlet/HelloServlet.java
@@ -0,0 +1,25 @@
+package com.xing.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    // 由于get或者post只是请求实现的不同方式，可以相互调用，业务逻辑都一样；
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("进入servlet");
+        PrintWriter writer = resp.getWriter(); //响应流
+        writer.print("Hello Servlet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
